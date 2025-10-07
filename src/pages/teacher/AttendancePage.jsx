a0770444ad4e2464669859a3a3c386f1da4cb156
EdwinT12 <122455780+EdwinT12@users.noreply.github.com>
2025-10-07 23:11:21 +0100

    Update AttendancePage.jsx

--- src/pages/teacher/AttendancePage.jsx
+++ src/pages/teacher/AttendancePage.jsx
@@ -3,7 +3,7 @@ import { useParams, useNavigate } from 'react-router';
 import { AuthContext } from '../../context/AuthContext';
 import supabase from '../../utils/supabase';
 import { batchSyncAttendance } from '../../utils/googleSheetsAPI';
-import toast from 'react-hot-toast';
+import toast from 'react-hot-toast'; 
 import { motion, AnimatePresence } from 'framer-motion';
 import { 
   Calendar, 
